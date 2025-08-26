79ee77d23271155edb21d177c17f99e69f466eaa
LearningMasterA <ankitasingh0263@gmail.com>
2025-08-26 23:54:51 +0530

    delete product page created

--- src/main/java/productcrudapp/controller/MainController.java
+++ src/main/java/productcrudapp/controller/MainController.java
@@ -1,11 +1,14 @@
 package productcrudapp.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.view.RedirectView;
@@ -19,7 +22,9 @@ public class MainController {
 	@Autowired
 	private ProductDao productDao;
 	@RequestMapping({"/","/home"})
-	public String home() {
+	public String home(Model m) {
+		List<Product>li=productDao.getProducts();
+		m.addAttribute("product",li);
 		return "index";
 	}
 //	Show add product form 
@@ -35,6 +40,17 @@ public class MainController {
 	public RedirectView handleProduct(@ModelAttribute Product product,HttpServletRequest request) {
 		productDao.createProduct(product);
 		System.out.println(product);
+		RedirectView rd=new RedirectView("add_product");
+		rd.setUrl(request.getContextPath()+"/");
+		return rd;
+	}
+	
+	
+//	Delete Handeler
+	
+	@RequestMapping("/delete/{productId}")
+	public RedirectView deleteProduct(@PathVariable("productId") int productId,HttpServletRequest request) {
+		this.productDao.deleteProduct(productId);
 		RedirectView rd=new RedirectView("add_product");
 		rd.setUrl(request.getContextPath()+"/");
 		return rd;
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -3,11 +3,62 @@
 <!DOCTYPE html>
 <html>
 <head>
+<%@include file="./base.jsp"  %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
 </head>
 <body>
-<h1>Welcome to the Home Page</h1>
+<div class="container mt-3">
+<div class="row">
+<div class="col-md-12">
+<h1 class="text-center">Welcome to Product App</h1>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Product Name</th>
+      <th scope="col">Product Description</th>
+      <th scope="col">Product Price</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  
+  <c:forEach items="${product}" var="p">
+    <tr>
+      <th scope="row">Hopes ${p.id }</th>
+      <td>${p.name }</td>
+      <td>${p.description }</td>
+      <td class="font-weight-bold"> &#8377 ${p.price}</td>
+      <td>
+    <a href="delete/${p.id}"><i class="fa-solid fa-trash text-danger" style="font-size:24px;"></i></a>
+    <a href="update/${p.id}"><i class="fa-solid fa-pen-nib text-primary" style="font-size:24px;"></i></a>
+
+
+      
+           
+      </td>
+      
+    </tr> 
+   </c:forEach>
+    
+  </tbody>
+</table>
+
+<div class="container text-center">
+<a href="addProduct" class="btn btn-outline-success">Add Product</a>
+
+
+</div>
+
+
+
+</div>
+</div>
+</div>
 
 </body>
 </html>
+
+
