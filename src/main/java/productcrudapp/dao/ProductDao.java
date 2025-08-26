6c3298c3ce6b93b8f370fa5edf7d2cebc1e71e16
LearningMasterA <ankitasingh0263@gmail.com>
2025-08-26 20:00:35 +0530

    tomcat server version changed from 10 to 9

--- src/main/java/productcrudapp/dao/ProductDao.java
+++ src/main/java/productcrudapp/dao/ProductDao.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.transaction.Transactional;
 import productcrudapp.model.Product;
 
 @Component
