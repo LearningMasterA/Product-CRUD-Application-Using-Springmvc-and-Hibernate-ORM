27c05fa8f41969ce3948872a1addc2ae6325215e
LearningMasterA <ankitasingh0263@gmail.com>
2025-08-26 22:50:12 +0530

    add product page created

--- src/main/java/productcrudapp/controller/MainController.java
+++ src/main/java/productcrudapp/controller/MainController.java
@@ -1,13 +1,43 @@
 package productcrudapp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
+import productcrudapp.dao.ProductDao;
+import productcrudapp.model.Product;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private ProductDao productDao;
 	@RequestMapping({"/","/home"})
 	public String home() {
 		return "index";
+	}
+//	Show add product form 
+	@RequestMapping("/addProduct")
+	public String addProduct(Model m) {
+		m.addAttribute("title","Add Product");
+		return "add_product_form";
+	}
+	
+	
+//	Handle add product form
+	@RequestMapping(value="/handle-product",method=RequestMethod.POST)
+	public RedirectView handleProduct(@ModelAttribute Product product,HttpServletRequest request) {
+		productDao.createProduct(product);
+		System.out.println(product);
+		RedirectView rd=new RedirectView("add_product");
+		rd.setUrl(request.getContextPath()+"/");
+		return rd;
 	}
 
 }
